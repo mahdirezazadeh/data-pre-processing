763bc2eb41d63deaed7b84c221d3306741576275
mahdi-win10 <riza.rs2000@gmail.com>
2022-01-28 22:49:09 +0330

    update source file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import fixData
 import perPopulation
 import t_test
 
-main_source_file_location = "source/SARS-CoV-2_Dataset.csv"
+main_source_file_location = "source/SARS-CoV-2 Dataset Updated.csv"
 cleaned_data_file_location = "output/cleaned_data.csv"
 per_population_data_file_location = "output/per_population_data.csv"
 t_test_file_location = "output/t_test.csv"
@@ -15,4 +15,4 @@ perPopulation.add_new_columns(cleaned_data_file_location, per_population_data_fi
 
 t_test.save_t_test(per_population_data_file_location, t_test_file_location)
 
-drawer.draw_charts(per_population_data_file_location)
+drawer.draw_charts(per_population_data_file_location, plots_output_directory)
